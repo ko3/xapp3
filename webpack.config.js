e255f9b1f328ff84b3315faae902da46f1e1bd99
ko3 <ek.lft15@gmail.com>
2018-07-16 18:10:18 -0500

    @Adding packaging file

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path:path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.jsx?$/,
+        loader: 'babel-loader',
+        query:{
+          presets: ['es2015', 'react']
+        }
+      },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+    ]
+  },
+  devServer:{
+    inline : true,
+    port : 8080,
+    historyApiFallback : true
+  },
+  plugins: [
+    //new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: __dirname + '/app/index.html',
+      filename: '/index.html',
+      inject: 'body'
+    })
+  ]
+};
